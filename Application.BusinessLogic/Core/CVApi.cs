350a6c5784f88ec1ac7b31d7c9841db8a74ff01b
stanislavbogatii <stas.bogattiy@gmail.com>
2024-05-24 01:58:59 +0300

    Update CV api, fix Collections update (skills, educations, experiences)

--- Application.BusinessLogic/Core/CVApi.cs
+++ Application.BusinessLogic/Core/CVApi.cs
@@ -16,7 +16,66 @@ namespace Application.BusinessLogic.Core
 {
     public class CVApi
     {
-       
+        public CreateCVResponse EditCVService(CV data, int cvId)
+        {
+            using (var db = new UserContext())
+            {
+                CVDbTable CV = db.CVs
+                           .Include(c => c.Skills)
+                           .Include(c => c.Experiences)
+                           .Include(c => c.Educations)
+                           .FirstOrDefault(c => c.Id == cvId);
+
+                if (CV != null)
+                {
+                    CV.Skills.Clear();
+                    CV.Experiences.Clear();
+                    CV.Educations.Clear();
+
+                    foreach (var skill in data.Skills)
+                    {
+                        CV.Skills.Add(skill);
+                    }
+                    foreach (var education in data.Educations)
+                    {
+                        CV.Educations.Add(education);
+                    }
+                    foreach (var experience in data.Experiences)
+                    {
+                        CV.Experiences.Add(experience);
+                    }
+
+
+                    db.SaveChanges();
+                    return new CreateCVResponse { IsSuccess = true, Msg = "Success edit a CV" };
+                }
+            }
+
+            return new CreateCVResponse { IsSuccess = false, Msg = "Failed edit a CV" };
+        }
+        public CreateCVResponse CreateCVService(CV data, string userEmail)
+        {
+            CVDbTable newCv = new CVDbTable
+            {
+                Educations = data.Educations,
+                Experiences = data.Experiences,
+                Skills = data.Skills,
+                Summary = data.Summary,
+                
+            };
+            using (var db = new UserContext())
+            {
+                UDbTable user = db.Users.FirstOrDefault(u => u.Email == userEmail);
+                if (user != null)
+                {
+                    user.CV = newCv;
+                    db.SaveChanges();
+                    return new CreateCVResponse { IsSuccess = true, Msg = "Success create a CV" };
+                }
+            }
+
+            return new CreateCVResponse { IsSuccess = false, Msg = "Failed create a CV" };
+        }
 
       
     }
